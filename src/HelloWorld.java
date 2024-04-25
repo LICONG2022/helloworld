73aa323fc1dae999598c15d89de8e1e45c7f25aa
cong <licong2020@qq.com>
2024-04-25 14:36:43 +0800

    本地库第一次提交

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("hello world");
-        System.out.println("远程仓库");
+        System.out.println("姓名：XXX");
+        System.out.println("班级：XXX");
+        System.out.println("学号：XXX");
     }
 }
